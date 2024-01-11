44cc18d9171e2efd5a929bea5e38b6d7fa2dcd88
Carlos Lespin <carlos.lespin.silva@gmail.com>
2024-01-11 04:36:33 -0500

    fix: cleaned up uneccesary endpoints

--- temples/ldstemples/urls.py
+++ temples/ldstemples/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('templelist/', views.templelist, name='templelist'),
     path('temple/<int:temple_id>/', views.temple, name='temple'),
     path('templeshow/<int:temple_id>/', views.templeshow, name='templeshow'),
 ]
--- temples/ldstemples/views.py
+++ temples/ldstemples/views.py
@@ -5,10 +5,6 @@ def home(request):
     temples = Temple.objects.all()
     return render(request, 'home.html', {"temples": temples})
 
-def templelist(request):
-    temples = Temple.objects.all()
-    return render(request, 'templelist.html', {"temples": temples})
-
 def temple(request, temple_id):
     temple = Temple.objects.get(id=temple_id)
     return render(request, 'temple.html', {"temple": temple})
